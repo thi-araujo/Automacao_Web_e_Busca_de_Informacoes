50ba045b7ce2ea12a8d18e1a232d9c8f82c7c21e
th-araujo <th.araujo@outlook.com>
2022-05-17 20:26:34 -0300

    "colocando o webdriver"

--- Automacao_Web_e_Busca_de_Informacoes.py
+++ Automacao_Web_e_Busca_de_Informacoes.py
@@ -1,3 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+
+# baixar o "chromedriver.exe" e comlocar na mesma psta onte esta instalado o python
+navegador = webdriver.Chrome()
+# navegador = webdriver.Chrome("chromedriver.exe") # -> quando o chromedriver estiver no mesmo local
+navegador.get("https://www.google.com.br/")
