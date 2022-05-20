1b4b0e80eefb3fdb1ab917d567ea797f3f488659
th-araujo <th.araujo@outlook.com>
2022-05-19 21:05:18 -0300

    "Abrir navegador Chrome pegar cotação do Dólar e Euro e fechar navegador"

--- Automacao_Web_e_Busca_de_Informacoes.py
+++ Automacao_Web_e_Busca_de_Informacoes.py
@@ -1,8 +1,41 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+# Automação Web e Busca de Informações com Python
 
-# baixar o "chromedriver.exe" e comlocar na mesma psta onte esta instalado o python
-navegador = webdriver.Chrome()
+# !pip install selenium -> Quando for usar no Jupyter Notebook instalar com esse comando
+
+# pip install selenium -> Quando usar  em uma IDE com Python instalar atraves desse comando pelo terminal
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+# Baixar o "chromedriver.exe" e colocar na mesma pasta onde esta instalado o python
+# Abrir um navegador
+navegador = webdriver.Chrome("chromedriver.exe")
 # navegador = webdriver.Chrome("chromedriver.exe") # -> quando o chromedriver estiver no mesmo local
 navegador.get("https://www.google.com.br/")
+
+
+# Passo 1: Pegar a cotação do Dólar
+navegador.find_element(By.XPATH,
+    '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("Cotação Dólar")
+
+navegador.find_element(By.XPATH,
+    '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
+
+cotacao_dolar = navegador.find_element(By.XPATH,
+    '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute("data-value")
+print(cotacao_dolar)
+
+# Passo 2: Pegar a cotação do Euro
+navegador.get("https://www.google.com/")
+navegador.find_element(By.XPATH,
+    '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("Cotação Euro")
+navegador.find_element(By.XPATH,
+    '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
+
+cotacao_euro = navegador.find_element(By.XPATH,
+    '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute("data-value")
+print(cotacao_euro)
+
+# Fechar o navegador
+navegador.quit()
